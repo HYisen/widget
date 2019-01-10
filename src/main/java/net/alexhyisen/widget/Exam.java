31f36ac8f3cbc395cf4e3ce2b7fc8cc59bb7ac46
Alex <alexhyisen@gmail.com>
2019-01-10 16:18:15 +0800

    add the ability to create in from origin

--- src/main/java/net/alexhyisen/widget/Exam.java
+++ src/main/java/net/alexhyisen/widget/Exam.java
@@ -1,17 +1,38 @@
 package net.alexhyisen.widget;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class Exam {
-    public static void main(String[] args) throws IOException {
-        var pass = 0;
-        var fail = 0;
-        var in = Files.readAllLines(Paths.get(".", "in"));
-        var out = Files.readAllLines(Paths.get(".", "out"));
-        for (int k = 0; k < in.size(); k++) {
+    private static void createIn() throws IOException {
+        var sheet = new XSSFWorkbook("origin.xlsx").getSheetAt(0);
+        var lines = StreamSupport
+                .stream(sheet.spliterator(), false)
+                .map(v -> String.format(
+                        "%s %s %s %s",
+                        v.getCell(0).getStringCellValue(),
+                        v.getCell(1).getStringCellValue(),
+                        v.getCell(2).getStringCellValue(),
+                        v.getCell(3).getStringCellValue())
+                )
+                .collect(Collectors.toList());
+        Files.write(Paths.get(".", "in"), lines);
+    }
 
+    private static void exam() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (!Files.exists(Paths.get(".", "in"))) {
+            createIn();
+        } else {
+            exam();
         }
     }
 }
